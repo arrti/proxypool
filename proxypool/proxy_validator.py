4239dc0dfb46abbdd6663ac421328fcb242e3e55
arrti <imxmwd@gmail.com>
2017-06-01 13:39:54 +0800

    update to support concurrency and some optimizations

--- proxypool/proxy_validator.py
+++ proxypool/proxy_validator.py
@@ -1,5 +1,5 @@
-from proxypool.config import validate_upper_limit, validate_ratio, \
-                             validate_cycle_time, validate_timeout
+from proxypool.config import (validate_upper_limit, validate_ratio,
+                              validate_cycle_time, validate_timeout, CORO_COUNT)
 from proxypool.db import RedisClient as rc
 from proxypool.utils import logger
 import asyncio
@@ -17,44 +17,46 @@ class ProxyValidator(object):
         # self._stop_flag = flag if flag else asyncio.Event()
         self._conn = conn
 
-    async def validate(self, proxies):
-        logger.debug('validator started')
+
+    async def _validator(self, proxy):
+        async with aiohttp.ClientSession() as session:
+            try:
+                real_proxy = 'http://' + proxy
+                async with session.get(self.validate_url, proxy=real_proxy, timeout=validate_timeout) as resp:
+                    self._conn.put(proxy)
+            except asyncio.TimeoutError:
+                pass
+            except Exception as e:
+                logger.error(e)
+
+    async def validate_many(self, proxies):
         while 1:
             proxy = await proxies.get()
-            async with aiohttp.ClientSession() as session:
-                try:
-                    real_proxy = 'http://' + proxy
-                    async with session.get(self.validate_url, proxy=real_proxy, timeout=validate_timeout) as resp:
-                        self._conn.put(proxy)
-                except Exception as e:
-                    logger.error(e)
-
+            await self._validator(proxy)
             proxies.task_done()
 
-    async def regular_validate(self):
+    async def _get_proxies(self):
         count = min(ceil(self._conn.count * validate_ratio), validate_upper_limit)
-        old_proxies = self._conn.get_list(count) # TODO: set an upper limit
-        valid_proxies = []
-        logger.debug('regular validator started, {0} to validate'.format(len(old_proxies)))
-        async with aiohttp.ClientSession() as session:
-            for proxy in old_proxies:
-                try:
-                    real_proxy = 'http://' + proxy.decode('utf-8') # proxy from redis was bytes type
-                    async with session.get(self.validate_url, proxy=real_proxy, timeout=validate_timeout) as resp:
-                        valid_proxies.append(proxy)
-                except asyncio.TimeoutError:
-                    continue
-                except Exception as e:
-                    logger.error(e)
+        old_proxies = self._conn.get_list(count)
+        valid_proxies = asyncio.Queue()
+        for proxy in old_proxies:
+            await valid_proxies.put(proxy.decode('utf-8'))
 
-        logger.debug('regular validator finished, {0} passed'.format(len(valid_proxies)))
-        self._conn.put_list(valid_proxies)
+        return valid_proxies
+
+    async def validate_one(self, proxies):
+        proxy = await proxies.get()
+        await self._validator(proxy)
+        proxies.task_done()
 
     async def start(self, proxies=None):
-        if proxies:
-            await self.validate(proxies)
+        if proxies is not None:
+            to_validate = [self.validate_many(proxies) for _ in range(CORO_COUNT)]
         else:
-            await self.regular_validate()
+            proxies = await self._get_proxies()
+            to_validate = [self.validate_one(proxies) for _ in range(proxies.qsize())]
+
+        await asyncio.wait(to_validate)
 
 
 def proxy_validator_run():
@@ -63,10 +65,12 @@ def proxy_validator_run():
     asyncio.set_event_loop(loop)
     validator = ProxyValidator(conn)
     while 1:
+        logger.debug('regular validator started')
         try:
             loop.run_until_complete(validator.start())
         except Exception as e:
             logger.error(e, exc_info=True)
+        logger.debug('regular validator finished')
         time.sleep(validate_cycle_time)
 
 
