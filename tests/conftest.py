dc5b46089065d2321d4c3c4b1b40eb30fa089af5
arrti <imxmwd@gmail.com>
2017-06-16 10:46:10 +0800

    update

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,19 +3,17 @@ import sys
 
 import pytest
 
+
 # add project path to sys.path in case 'proxypool' was not installed
 current = Path(__file__).parent.parent
 path = str(current)
 sys.path.append(path)
-
 
 def pytest_addoption(parser):
     parser.addoption("--runslow", action="store_true",
                      help="run slow tests")
     parser.addoption("--runptjs", action="store_true",
                      help="run tests using phantomjs")
-
-
 
 @pytest.fixture(scope='session')
 def db():
